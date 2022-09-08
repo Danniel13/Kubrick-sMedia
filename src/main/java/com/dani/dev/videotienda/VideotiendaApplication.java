0c3da7f496d27a97cf702ea68a83aae69a3f3862
Danniel Gonzalez <danielgz.m@hotmail.com>
2022-09-07 21:30:07 -0500

    Commit inicial

---
+++ src/main/java/com/dani/dev/videotienda/VideotiendaApplication.java
@@ -0,0 +1,13 @@
+package com.dani.dev.videotienda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideotiendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideotiendaApplication.class, args);
+	}
+
+}
