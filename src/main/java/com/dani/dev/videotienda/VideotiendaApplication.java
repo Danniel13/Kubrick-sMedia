824e5fca395fd4f318815b6a96e41231e871d6f0
Danniel Gonzalez <danielgz.m@hotmail.com>
2022-09-07 21:49:37 -0500

    Add body index

--- src/main/java/com/dani/dev/videotienda/VideotiendaApplication.java
+++ src/main/java/com/dani/dev/videotienda/VideotiendaApplication.java
@@ -2,7 +2,7 @@ package com.dani.dev.videotienda;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+// Clase que va a permitir ejecutar proyecto, agregar configs iniciales del proyecto
 @SpringBootApplication
 public class VideotiendaApplication {
 
