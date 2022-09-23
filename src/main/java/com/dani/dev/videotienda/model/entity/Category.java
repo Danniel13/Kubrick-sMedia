421b139397ff6cbfa10900dd2cd75a08bac3361e
Danniel Gonzalez <danielgz.m@hotmail.com>
2022-09-22 21:33:19 -0500

    cambios

--- src/main/java/com/dani/dev/videotienda/model/entity/Category.java
+++ src/main/java/com/dani/dev/videotienda/model/entity/Category.java
@@ -2,11 +2,23 @@ package com.dani.dev.videotienda.model.entity;
 
 import java.util.List;
 
+import javax.annotation.Generated;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import lombok.Data;
 @Entity
 @Data //Importa set & get de los atributos.
 public class Category {
+
+
+
+ @Id
+ @GeneratedValue(strategy = GenerationType.AUTO)  //->determiar que el id se genera automaticamente en BD.
+  private Long id; //->Id de base de datos llave primaria de tabla
+
   private String name;
 
   private List<Movie> movies; //Relacion una categoria a muchas peliculas
