1416473c1e37f59e205c54dc0aeb72665512375d
Danniel Gonzalez <danielgz.m@hotmail.com>
2022-09-24 08:57:41 -0500

    category

--- src/main/java/com/dani/dev/videotienda/model/entity/Category.java
+++ src/main/java/com/dani/dev/videotienda/model/entity/Category.java
@@ -3,6 +3,7 @@ package com.dani.dev.videotienda.model.entity;
 import java.util.List;
 
 import javax.annotation.Generated;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -19,6 +20,8 @@ public class Category {
  @GeneratedValue(strategy = GenerationType.AUTO)  //->determiar que el id se genera automaticamente en BD.
   private Long id; //->Id de base de datos llave primaria de tabla
 
+
+  @Column(name= "nombre", nullable = false, length = 100)//Caracteristicas de campo en BD
   private String name;
 
   private List<Movie> movies; //Relacion una categoria a muchas peliculas
